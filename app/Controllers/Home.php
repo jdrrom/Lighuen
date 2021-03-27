7f550eca33d61ddfa027db275d1b0540be40486f
Diego <jorge.diego.rom@gmail.com>
2021-03-27 19:46:14 -0300

    funciona precios con armas con iconos

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,14 +9,32 @@ class Home extends BaseController
 	public function index()
 	{
 		return view('welcome_message');
-	}
+    }
 
 	public function test(){
-	    $url = "http://csgobackpack.net/api/GetItemPrice/?currency=USD&id=AK-47%20|%20Wasteland%20Rebel%20(Battle-Scarred)&time=7&icon=1";
+	    $items = [
+	        "StatTrak™%20Desert%20Eagle%20%7C%20Corinthian%20%28Minimal%20Wear%29",
+            "StatTrak™%20Desert%20Eagle%20%7C%20Blue%20Ply%20%28Factory%20New%29",
+            "StatTrak™%20Desert%20Eagle%20%7C%20Mecha%20Industries%20%28Field-Tested%29",
+            "StatTrak™%20Desert%20Eagle%20%7C%20Conspiracy%20%28Minimal%20Wear%29"
+            ];
+	    $urlBase = "http://csgobackpack.net/api/GetItemPrice/?currency=ARS&id=";
+        $urlParameters = "&time=7&icon=1";
         $client = Services::curlrequest();
-        $response = $client->request('GET', $url);
+        $array = [];
+
+        foreach ($items as $item){
+            $response = $client->request('GET', $urlBase.$item.$urlParameters);
+            $respuesta = json_decode($response->getBody(),true);
+
+            if($respuesta["icon"]!=""){
+                array_push($array,$respuesta);
+            }
+        }
+
         $data = [
-            "respone" => $response->getBody()
+            "response" => $array,
+
         ];
         return view('testView',$data);
     }
--- app/Views/testView.php
+++ app/Views/testView.php
@@ -1,1 +1,11 @@
-<?php echo $respone;?>
+<h1>Precios promedios</h1>
+<?php
+    foreach ($response as $item){
+?>
+        <div><img src="<?php echo $item["icon"]?>"></div>
+        <div><h2>El precio es: <?php echo $item["average_price"]?></h2></div>
+        <br>
+
+<?php
+    }
+?>
