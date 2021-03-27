fb3bfcd03fb60d4641e1b6e0613c22ad90a36c45
ldiazmininterior <ldiaz@mininterior.gob.ar>
2021-03-27 17:46:47 -0300

    se agrega prueba de request

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,22 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Config\Services;
+
 class Home extends BaseController
 {
 	public function index()
 	{
 		return view('welcome_message');
 	}
+
+	public function test(){
+	    $url = "http://csgobackpack.net/api/GetItemPrice/?currency=USD&id=AK-47%20|%20Wasteland%20Rebel%20(Battle-Scarred)&time=7&icon=1";
+        $client = Services::curlrequest();
+        $response = $client->request('GET', $url);
+        $data = [
+            "respone" => $response->getBody()
+        ];
+        return view('testView',$data);
+    }
 }
