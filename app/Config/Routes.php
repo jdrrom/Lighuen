0eb2e61233f3fede99fc1542bbe9f2f13b2be779
Diego <jorge.diego.rom@gmail.com>
2021-03-27 21:12:53 -0300

    lista automatica de 10 elementos

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,6 +35,7 @@ $routes->setAutoRoute(true);
 $routes->get('/', 'Home::index');
 $routes->get('/test', 'Home::test');
 $routes->get('/todo', 'Home::todo');
+$routes->get('/traerItems', 'Home::traerItems');
 
 /*
  * --------------------------------------------------------------------
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -12,12 +12,7 @@ class Home extends BaseController
     }
 
 	public function test(){
-	    $items = [
-	        "StatTrak™%20Desert%20Eagle%20%7C%20Corinthian%20%28Minimal%20Wear%29",
-            "StatTrak™%20Desert%20Eagle%20%7C%20Blue%20Ply%20%28Factory%20New%29",
-            "StatTrak™%20Desert%20Eagle%20%7C%20Mecha%20Industries%20%28Field-Tested%29",
-            "StatTrak™%20Desert%20Eagle%20%7C%20Conspiracy%20%28Minimal%20Wear%29"
-            ];
+	    $items = json_decode($this->traerItems());
 	    $urlBase = "http://csgobackpack.net/api/GetItemPrice/?currency=ARS&id=";
         $urlParameters = "&time=7&icon=1";
         $client = Services::curlrequest();
@@ -27,7 +22,7 @@ class Home extends BaseController
             $response = $client->request('GET', $urlBase.$item.$urlParameters);
             $respuesta = json_decode($response->getBody(),true);
 
-            if($respuesta["icon"]!=""){
+            if(isset($respuesta["icon"]) && $respuesta["icon"]!=""){
                 array_push($array,$respuesta);
             }
         }
@@ -56,4 +51,21 @@ class Home extends BaseController
         return json_encode($arrayFiltrado);
     }
 
+    public function traerItems(){
+        $url ="http://csgobackpack.net/api/GetItemsList/v2/?no_details=1&currency=ARS";
+        $client = Services::curlrequest();
+        $response = $client->request('GET', $url);
+        $respuesta = json_decode($response->getBody(),true)["items_list"];
+        $arrayFiltrado = [];
+        foreach ($respuesta as $item){
+            if(isset($item["price"])){
+                array_push($arrayFiltrado,rawurlencode($item["name"]));
+            }
+        }
+        $finalArray = [];
+        foreach (array_rand ($arrayFiltrado,10) as $element){
+            array_push($finalArray,$arrayFiltrado[$element]);
+        }
+	    return json_encode($finalArray);
+    }
 }
